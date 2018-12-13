116867c206cae19e5be7e295d43a776670d97dfb
Rizki Achmad R <kkrizki00@gmail.com>
2018-12-13 12:23:39 +0700

    penjualan
    1/4 jadi

--- RaniahFood/src/raniahfood/model/Penjualan.java
+++ RaniahFood/src/raniahfood/model/Penjualan.java
@@ -14,13 +14,57 @@ import java.util.*;
 public class Penjualan {
     private String id_penjualan;
     String tgl_penjualan;
-    int jumlah_penjualan;
-    List ListProduk = new ArrayList();
     String nama_pelanggan;
     String alamat_pelanggan;
-    String hp_pelanggan;
+    int uang;
+
+    public Penjualan(String id_penjualan, String tgl_penjualan, String nama_pelanggan, String alamat_pelanggan, int uang) {
+        this.id_penjualan = id_penjualan;
+        this.tgl_penjualan = tgl_penjualan;
+        this.nama_pelanggan = nama_pelanggan;
+        this.alamat_pelanggan = alamat_pelanggan;
+        this.uang = uang;
+    }
+
+    public String getId_penjualan() {
+        return id_penjualan;
+    }
+
+    public void setId_penjualan(String id_penjualan) {
+        this.id_penjualan = id_penjualan;
+    }
+
+    public String getTgl_penjualan() {
+        return tgl_penjualan;
+    }
+
+    public void setTgl_penjualan(String tgl_penjualan) {
+        this.tgl_penjualan = tgl_penjualan;
+    }
+
+    public String getNama_pelanggan() {
+        return nama_pelanggan;
+    }
+
+    public void setNama_pelanggan(String nama_pelanggan) {
+        this.nama_pelanggan = nama_pelanggan;
+    }
+
+    public String getAlamat_pelanggan() {
+        return alamat_pelanggan;
+    }
+
+    public void setAlamat_pelanggan(String alamat_pelanggan) {
+        this.alamat_pelanggan = alamat_pelanggan;
+    }
+
+    public int getUang() {
+        return uang;
+    }
+
+    public void setUang(int uang) {
+        this.uang = uang;
+    }
     
-    public Penjualan(String id, String nama, int stok, int harga) {
-        this.id_penjualan = id;
-    }    
+
 }
